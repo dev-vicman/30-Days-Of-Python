e890c627be3162ffa34cd0d65022350a7ea376a3
Victor Hernandez <vmhm182@gmail.com>
2024-08-24 15:01:28 -0600

    Inicio de dia 7

---
+++ day_7/sets.py
@@ -0,0 +1,26 @@
+""" Day 7: 30 Days of python programming """
+
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+""" Ejercicios: Nivel 1
+Encuentra la longitud del conjunto it_companies
+Añade 'Twitter' a it_companies
+Insertar varias empresas de TI a la vez en el conjunto it_companies
+Eliminar una de las empresas del conjunto it_companies
+¿Cuál es la diferencia entre eliminar y descartar?
+Ejercicios: Nivel 2
+Unir A y B
+Encuentra la intersección A y B
+Es un subconjunto de B
+¿Son A y B conjuntos disjuntos?
+Unir A con B y B con A
+¿Cuál es la diferencia simétrica entre A y B?
+Eliminar los conjuntos por completo
+Ejercicios: Nivel 3
+Convierte las edades en un conjunto y compara la longitud de la lista y el conjunto, ¿cuál es más grande?
+Explique la diferencia entre los siguientes tipos de datos: cadena, lista, tupla y conjunto.
+Soy profesor y me encanta inspirar y enseñar a la gente. ¿Cuántas palabras únicas se han utilizado en la oración? Utilice los métodos de división y de configuración para obtener las palabras únicas."""
