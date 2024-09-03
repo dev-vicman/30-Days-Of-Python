94f20ce8b02743348151cdb683eac1f129181516
Victor Hernandez <vmhm182@gmail.com>
2024-09-03 16:35:44 -0600

    Iniciando el dia 12

---
+++ day_12/modulos.py
@@ -0,0 +1,49 @@
+""" Day 12: 30 Days of python programming """
+
+#? Ejercicios: Nivel 1
+
+""" 1. Escriba una función que genere un random_user_id de seis dígitos/caracteres.
+    #print(random_user_id());
+    #'1ee33d' """
+    
+    
+""" 2. Modifique la tarea anterior. Declare una función llamada user_id_gen_by_user. No acepta ningún parámetro, pero acepta dos entradas mediante input(). Una de las entradas es la cantidad de caracteres y la segunda es la cantidad de identificaciones que se supone que se generarán.
+    print(user_id_gen_by_user()) # user input: 5 5
+    output:
+    kcsy2
+    SMFYb
+    bWmeq
+    ZXOYh
+    2Rgxf
+
+    print(user_id_gen_by_user()) # 16 5
+    1GCSgPLMaBAVQZ26
+    YD7eFwNQKNs7qXaT
+    ycArC5yrRupyG00S
+    UbGxOFI7UXSWAyKN
+    dIV0SSUTgAdKwStr """
+
+""" 3. Escriba una función llamada rgb_color_gen. Generará colores RGB (tres valores que van de 0 a 255 cada uno).
+#print(rgb_color_gen())
+# rgb(125,244,255) - the output should be in this form """
+
+#? Ejercicios: Nivel 2
+
+""" 4. Escriba una función list_of_hexa_colors que devuelva cualquier número de colores hexadecimales en una matriz (seis números hexadecimales escritos después de #. El sistema de numeración hexadecimal está formado por 16 símbolos, del 0 al 9 y las primeras 6 letras del alfabeto, af. Consulte la tarea 6 para ver ejemplos de salida). """
+
+""" 5. Escriba una función list_of_rgb_colors que devuelva cualquier número de colores RGB en una matriz. """
+
+""" 6. Escriba una función generate_colors que pueda generar cualquier cantidad de colores hexadecimales o rgb.
+    generate_colors('hexa', 3) # ['#a3e12f','#03ed55','#eb3d2b'] 
+    generate_colors('hexa', 1) # ['#b334ef']
+    generate_colors('rgb', 3)  # ['rgb(5, 55, 175','rgb(50, 105, 100','rgb(15, 26, 80'] 
+    generate_colors('rgb', 1)  # ['rgb(33,79, 176)'] """
+
+#? Ejercicios: Nivel 3
+
+""" 7. Llama a tu función shuffle_list, toma una lista como parámetro y devuelve una lista aleatoria """
+
+
+""" 8. Escriba una función que devuelva una matriz de siete números aleatorios en un rango de 0 a 9. Todos los números deben ser únicos. """
+
+
