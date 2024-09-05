f3f38c579b7e07c5cc5ec0c036cddd9ed22e2714
Victor Hernandez <vmhm182@gmail.com>
2024-09-05 10:42:52 -0600

    Resolviendo ejercicios dia 12

--- day_12/modulos.py
+++ day_12/modulos.py
@@ -1,35 +1,53 @@
 """ Day 12: 30 Days of python programming """
 
+# Importar modulos
+import random
+import string
+
+# print(randint(1,100))
+
 #? Ejercicios: Nivel 1
 
 """ 1. Escriba una función que genere un random_user_id de seis dígitos/caracteres.
     #print(random_user_id());
     #'1ee33d' """
-    
-    
-""" 2. Modifique la tarea anterior. Declare una función llamada user_id_gen_by_user. No acepta ningún parámetro, pero acepta dos entradas mediante input(). Una de las entradas es la cantidad de caracteres y la segunda es la cantidad de identificaciones que se supone que se generarán.
-    print(user_id_gen_by_user()) # user input: 5 5
-    output:
-    kcsy2
-    SMFYb
-    bWmeq
-    ZXOYh
-    2Rgxf
 
-    print(user_id_gen_by_user()) # 16 5
-    1GCSgPLMaBAVQZ26
-    YD7eFwNQKNs7qXaT
-    ycArC5yrRupyG00S
-    UbGxOFI7UXSWAyKN
-    dIV0SSUTgAdKwStr """
+def random_user_id():
+    # Generar una combinación de dígitos y letras minúsculas
+    lista = string.digits + string.ascii_lowercase
+    
+    # Inicializar una lista para almacenar los caracteres seleccionados
+    caracteres_seleccionados = []
+    
+    # Seleccionar seis caracteres aleatorios de la lista
+    for _ in range(6):
+        caracter_aleatorio = random.choice(lista)
+        caracteres_seleccionados.append(caracter_aleatorio)
+    
+    # Unir los caracteres seleccionados en una sola cadena
+    cadena = ''.join(caracteres_seleccionados)
+    
+    return cadena
 
-""" 3. Escriba una función llamada rgb_color_gen. Generará colores RGB (tres valores que van de 0 a 255 cada uno).
+print('Clave:', random_user_id())
+
+""" 2. Escriba una función llamada rgb_color_gen. Generará colores RGB (tres valores que van de 0 a 255 cada uno).
 #print(rgb_color_gen())
 # rgb(125,244,255) - the output should be in this form """
 
+print(random.randint(1,255))
+
+def rgb_color_gen():
+    return f"rgb({random.randint(1,255)},{random.randint(1,255)},{random.randint(1,255)})"
+
+print(rgb_color_gen())
+
+
 #? Ejercicios: Nivel 2
 
-""" 4. Escriba una función list_of_hexa_colors que devuelva cualquier número de colores hexadecimales en una matriz (seis números hexadecimales escritos después de #. El sistema de numeración hexadecimal está formado por 16 símbolos, del 0 al 9 y las primeras 6 letras del alfabeto, af. Consulte la tarea 6 para ver ejemplos de salida). """
+""" 3. Escriba una función list_of_hexa_colors que devuelva cualquier número de colores hexadecimales en una matriz (seis números hexadecimales escritos después de #. El sistema de numeración hexadecimal está formado por 16 símbolos, del 0 al 9 y las primeras 6 letras del alfabeto, af. Consulte la tarea 6 para ver ejemplos de salida). """
+
+
 
 """ 5. Escriba una función list_of_rgb_colors que devuelva cualquier número de colores RGB en una matriz. """
 
